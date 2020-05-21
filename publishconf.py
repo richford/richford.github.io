b94b4d8405559aa9fb6064898e640f0382a29072
Adam Richie-Halford <richford@uw.edu>
2020-05-21 04:14:28 -0700

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,12 +10,13 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.richiehalford.com'
 THEME = '/Users/richford/Documents/website/pelican-subtle'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 PAGE_ORDER_BY = 'sortorder'
